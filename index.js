f308c1728341575a329dace2d96f0c1fe4de30f2
Finn <derfinn@gmail.com>
2014-09-10 12:49:51 +0200

    passing basic test suite

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var util = require('util')
 var request = require('request')
 var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN
+var sql = require('pg-escape')
+var qs = require('querystring')
 
 function ckanDOWN (location) {
   if(!(this instanceof ckanDOWN)) return new ckanDOWN(location)
   AbstractLevelDOWN.call(this, location)
-  this.resourceID = '37e5da24-90d5-4e57-b900-df6dd960c38d'
-  this.apikey = 'cc03492e-11b2-4ff5-aa81-148e6bb3da18'
-  this.headers = {
-    'content-type': 'application/json',
-    'Authorization': this.apikey
-  }
+  var parts = this.location.split('?')
+  this.location = parts[0]
+  // remove / in the end
+  if(this.location[this.location.length-1] === '/')
+    this.location = this.location.slice(0,-1)
+  // opts
+  var opts = qs.parse(parts[1])
+  this.resourceID = opts.resource // e.g '37e5da24-90d5-4e57-b900-df6dd960c38d'
+  this.apikey = opts.apikey // e.g. 'cc03492e-11b2-4ff5-aa81-148e6bb3da18'
 }
 
 util.inherits(ckanDOWN, AbstractLevelDOWN)
@@ -20,7 +25,10 @@ ckanDOWN.prototype._open = function (options, callback) {
   // set the correct layout for the resource
   request({
     url: this.location + '/api/3/action/datastore_create',
-    headers: this.headers,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': this.apikey
+    },
     method: 'POST',
     
     json: {
@@ -39,6 +47,7 @@ ckanDOWN.prototype._open = function (options, callback) {
       primary_key: 'key'
     }
   }, function (err, response, body) {
+    // console.log(body)
     if(err) return callback(err)
     if(response.statusCode !== 200) return callback(new Error('StatusCode ' + response.statusCode))
     callback()
@@ -53,15 +62,18 @@ ckanDOWN.prototype._put = function (key, value, options, callback) {
   var params = {
     resource_id: this.resourceID,
     records: [{
-      key: key,
-      value: value
+      key: key.toString(),
+      value: value.toString()
     }],
     force: 'True', // is there another way?
     method: 'upsert' // for 'upsert' i need to set a key
   }
   request({
     url: url,
-    headers: this.headers,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': this.apikey
+    },
     json: params,
     method: 'POST' // or PUT? who knows fuck the documentation
   }, function (err, response, body) {
@@ -76,7 +88,7 @@ ckanDOWN.prototype._put = function (key, value, options, callback) {
 ckanDOWN.prototype._get = function (key, options, callback) {
   //http://{YOUR-CKAN-INSTALLATION}/api/3/action/datastore_search
   var url = this.location + '/api/3/action/datastore_search_sql'
-  var sqlQuery = 'SELECT key, value FROM "' + this.resourceID + '" WHERE key = \'' + key + '\''
+  var sqlQuery = sql('SELECT key, value FROM %I WHERE key = %L', this.resourceID, key.toString())
   request({
     url: url,
     qs: {
@@ -86,8 +98,11 @@ ckanDOWN.prototype._get = function (key, options, callback) {
     if(err) return callback(err)
     if(response.statusCode !== 200) return callback(new Error('StatusCode ' + response.statusCode))
     var data = JSON.parse(body)
-    if(data.result.records.length === 0) return callback(new Error('Not Found'))
-    callback(null, data.result.records[0].value)
+    if(data.result.records.length === 0) return callback(new Error('NotFound'))
+    var result = data.result.records[0].value
+    if(options.asBuffer)
+      result = new Buffer(result)
+    callback(null, result)
   })
   
 }
@@ -96,7 +111,6 @@ ckanDOWN.prototype._del = function (key, options, callback) {
   // delete
   // debug curl -X POST http://demo.ckan.org/api/3/action/datastore_delete -H "Authorization: cc03492e-11b2-4ff5-aa81-148e6bb3da18" -d '{"resource_id": "37e5da24-90d5-4e57-b900-df6dd960c38d", "filters": {"key": "aberwas"}, "force": "True"}'
   var url = this.location + '/api/3/action/datastore_delete'
-  console.log(this.headers)
   request({
     url: url,
     method: 'POST',
@@ -108,27 +122,15 @@ ckanDOWN.prototype._del = function (key, options, callback) {
       resource_id: this.resourceID,
       force: 'True',
       filters: {
-        key: key
+        key: key.toString()
       }
     }
   }, function (err, response, body) {
     if(err) return callback(err)
-    console.log(body)
     if(response.statusCode !== 200) return callback(new Error('StatusCode ' + response.statusCode))
     callback()
   })
 
 }
 
-ckanDOWN.prototype._ckancall = function (resource) {
-  var url = this.location = this.location + '/api/3/action/' + resource
-  
-  
-  //datastore_upsert
-}
-
-
-
 module.exports = ckanDOWN
-
-
--- test.js
+++ test.js
@@ -1,29 +1,37 @@
-// var test = require('tap').test
-// var testCommon = require('abstract-leveldown/testCommon')
+var test = require('tape')
+var testCommon = require('abstract-leveldown/testCommon')
 var ckanDOWN = require('./')
 
-// require('abstract-leveldown/abstract/open-test').args(ckanDOWN, test, testCommon)
-// require('abstract-leveldown/abstract/open-test').open(ckanDOWN, test, testCommon)
-// 
-// require('abstract-leveldown/abstract/put-test').all(ckanDOWN, test, testCommon)
+testCommon.location = function () {
+  return 'http://demo.ckan.org/?resource=37e5da24-90d5-4e57-b900-df6dd960c38d&apikey=cc03492e-11b2-4ff5-aa81-148e6bb3da18'
+}
 
-var smalltest = ckanDOWN('http://demo.ckan.org')
+require('abstract-leveldown/abstract/open-test').args(ckanDOWN, test, testCommon)
+require('abstract-leveldown/abstract/open-test').open(ckanDOWN, test, testCommon)
+ 
+require('abstract-leveldown/abstract/put-test').all(ckanDOWN, test, testCommon)
+require('abstract-leveldown/abstract/get-test').all(ckanDOWN, test, testCommon)
+require('abstract-leveldown/abstract/del-test').all(ckanDOWN, test, testCommon)
+require('abstract-leveldown/abstract/put-get-del-test').all(ckanDOWN, test, testCommon, new Buffer('testbuffer'))
+
+
+// var smalltest = ckanDOWN('http://demo.ckan.org')
 
 // smalltest.create(function () {
 //   console.log('done')
 // })
 // 
 // 
-smalltest.put('aberwas', 'blub', function (err) {
-  smalltest.get('aberwas',function (err, value) {
-    if(err) console.error(err)
-    console.log(value)
-    smalltest.del('aberwas', function (err) {
-      if(err) console.error(err)
-      console.log('deleted')
-    })
-  })
-})
+// smalltest.put('aberwas', 'blub', function (err) {
+//   smalltest.get('aberwas',function (err, value) {
+//     if(err) console.error(err)
+//     console.log(value)
+//     smalltest.del('aberwas', function (err) {
+//       if(err) console.error(err)
+//       console.log('deleted')
+//     })
+//   })
+// })
 
 // smalltest.del('aberwas', function (err) {
 //   if(err) console.error(err)
