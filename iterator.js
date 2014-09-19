29e3a66fb8fe57a3dfaa8d64c91ed3c6258ef967
Finn <derfinn@gmail.com>
2014-09-19 19:36:41 +0200

    holy shit, tests are passing

--- iterator.js
+++ iterator.js
@@ -5,8 +5,53 @@ var request = require('request')
 
 function ckanIterator(db, options) {
   AbstractIterator.call(this, db)
+  
+  if(options.keyAsBuffer === false) this._keyAsBuffer = false
+  else this._keyAsBuffer = true
+  if(options.valueAsBuffer === false) this._valueAsBuffer = false
+  else this._valueAsBuffer = true
+    
   this.url = db.location + '/api/3/action/datastore_search_sql'
-  this.sqlQuery = sql('SELECT key, value FROM %I ORDER BY key ASC', db.resourceID)
+  var order = this.reverse ? 'DESC' : 'ASC'
+  
+  var start = options.start
+  var end = options.end
+  
+  var query = ['SELECT key, value FROM %I']
+  var queryValues = [db.resourceID]
+  
+  if (options.reverse) {
+      if (start && end){
+        query.push('WHERE key <= %L AND key >= %L')
+        queryValues.push(start, end)
+      } else if (start) {
+        query.push('WHERE key <= %L')
+        queryValues.push(start)
+      } 
+      else if (end) {
+        query.push('WHERE key >= %L')
+        queryValues.push(end)
+      }
+      query.push('ORDER BY key DESC')
+    } else {
+      if (start && end) {
+        query.push('WHERE key >= %L AND key <= %L')
+        queryValues.push(start, end)
+      } else if (start) {
+        query.push('WHERE key >= %L')
+        queryValues.push(start)
+      } else if (end) {
+        query.push('WHERE key <= %L')
+        queryValues.push(end)  
+      }
+      query.push('ORDER BY key ASC')
+    }
+    
+  if ((options.limit && options.limit !== -1) || options.limit === 0) {
+    query.push('LIMIT ' + options.limit)
+  }
+  
+  this.sqlQuery = sql.apply(null, [query.join(' ')].concat(queryValues))
   this.fetched = []
   this.fetchedAll = false
 }
@@ -23,7 +68,6 @@ ckanIterator.prototype._fetch = function (callback) {
     if(err) return callback(err)
     if(response.statusCode !== 200) return callback(new Error('StatusCode ' + response.statusCode))
     var data = JSON.parse(body)
-    if(data.result.records.length === 0) return callback(new Error('NotFound'))
     this.fetched = this.fetched.concat(data.result.records)
     this.fetchedAll = true // does this API use/allow pagination?
     callback(null)
@@ -42,7 +86,9 @@ ckanIterator.prototype._next = function (callback) {
   } else {
     if(this.fetched.length === 0) return callback()
     var current = this.fetched.shift()
-    callback(null, new Buffer(current.key), new Buffer(current.value))
+    if(this._keyAsBuffer) current.key = new Buffer(current.key)
+    if(this._valueAsBuffer) current.value = new Buffer(current.value)
+    callback(null, current.key, current.value)
   }
   
 }
--- test.js
+++ test.js
@@ -4,40 +4,71 @@ var ckanDOWN = require('./')
 var request = require('request')
 
 var apikey = 'cc03492e-11b2-4ff5-aa81-148e6bb3da18'
+
+
 // TODO: Automatic creation of the test resource
-var testresource = '4363f707-2796-4882-9e2d-63eb77259740'
+testCommon._resources = 
+  [
+    '4363f707-2796-4882-9e2d-63eb77259740',
+    'd65cd314-d7dd-4aaa-995b-907f9169f0d2',
+    'af2899f3-3df5-4764-90d4-d4682ca45a9e',
+    '9bfc7ea8-f9f2-4a19-a4bd-0a86c43a45bb',
+    '48438294-16d1-4ac4-87c1-0c73defcf0a5'
+  ]
+  
+testCommon._locationid = 0
+
+testCommon._resource = function () {
+  if(!(this._locationid in this._resources)) this._locationid = 0
+  return this._resources[this._locationid]
+}
 
 testCommon.location = function () {
-  return 'http://demo.ckan.org/?resource=' + testresource + '&apikey=' + apikey
+  var resource = this._resource()
+  this._locationid++
+  return 'http://demo.ckan.org/?resource=' + resource + '&apikey=' + apikey
 }
 
 testCommon.cleanup = function (callback) {
-  request({
-    url: 'http://demo.ckan.org/api/3/action/datastore_delete',
-    headers: {
-      'content-type': 'application/json',
-      'Authorization': apikey
-    },
-    method: 'POST',
-    json: {
-      'resource_id': testresource,
-      'force': true,
-      'filters': {}
-      }
-  }, function (err, response, body) {
-    if(err) throw err
-    callback()
+  var countdown = this._resources.length
+  this._resources.forEach(function (resource) {
+    request({
+      url: 'http://demo.ckan.org/api/3/action/datastore_delete',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': apikey
+      },
+      method: 'POST',
+      json: {
+        'resource_id': resource,
+        'force': true,
+        'filters': {}
+        }
+    }, function (err, response, body) {
+      if(err) throw err
+      countdown--
+      if(countdown === 0)
+        callback()
+    })
   })
 }
 
-require('abstract-leveldown/abstract/open-test').args(ckanDOWN, test, testCommon)
-require('abstract-leveldown/abstract/open-test').open(ckanDOWN, test, testCommon)
- 
-require('abstract-leveldown/abstract/put-test').all(ckanDOWN, test, testCommon)
-require('abstract-leveldown/abstract/get-test').all(ckanDOWN, test, testCommon)
-require('abstract-leveldown/abstract/del-test').all(ckanDOWN, test, testCommon)
-require('abstract-leveldown/abstract/put-get-del-test').all(ckanDOWN, test, testCommon, new Buffer('testbuffer'))
+testCommon.cleanup(function () {
+  // not compatible with snapshot
+  require('abstract-leveldown/abstract/iterator-test').setUp(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/iterator-test').args(test)
+  require('abstract-leveldown/abstract/iterator-test').sequence(test)
+  require('abstract-leveldown/abstract/iterator-test').iterator(ckanDOWN, test, testCommon, testCommon.collectEntries)
+  require('abstract-leveldown/abstract/iterator-test').tearDown(test, testCommon)
 
-require('abstract-leveldown/abstract/batch-test').all(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/open-test').args(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/open-test').open(ckanDOWN, test, testCommon)
+   
+  require('abstract-leveldown/abstract/put-test').all(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/get-test').all(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/del-test').all(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/put-get-del-test').all(ckanDOWN, test, testCommon, new Buffer('testbuffer'))
 
-// require('abstract-leveldown/abstract/iterator-test').all(ckanDOWN, test, testCommon)
+  require('abstract-leveldown/abstract/batch-test').all(ckanDOWN, test, testCommon)
+})
+
